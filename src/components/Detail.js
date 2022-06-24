2d098e4370732c41cf7d1ada777254c5e9822b6f
Leos <highwind26@gmail.com>
2022-06-24 22:35:28 +0900

    Detail 마무리

--- src/components/Detail.js
+++ src/components/Detail.js
@@ -1,4 +1,5 @@
 import React from "react";
+import '../css/Detail.css'
 
 // Class Type Component 
 class Detail extends React.Component {
@@ -16,14 +17,28 @@ class Detail extends React.Component {
 
         const { location } = this.props;
         if (location.state) {
+            const movieDetail = location.state;
             return (
-                    <span>{ location.state.title }</span>
+                    <div className="movie__detail">
+                        <img src={movieDetail.poster} alt={movieDetail.title} />
+                        <div className="movie__data">
+                            <h3 className="movie__title">{movieDetail.title}</h3>
+                            <h5 className="movie__year">{movieDetail.year}</h5>
+                            <ul className="movie__genres">
+                                {movieDetail.genres.map((genre, index) => {
+                                    return <li key={index} className="movie__genre">{genre}</li>
+                                })}
+                            </ul>
+                            <hr/>
+                            <p className="movie__summary">{movieDetail.summary}</p>
+                        </div>
+                    </div>
                    )
         } else {
             return null;
         }
 
-    }
+    } // END render()
 
 }
 
